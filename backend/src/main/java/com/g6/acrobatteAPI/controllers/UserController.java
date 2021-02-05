4825572863cff321169038ba898ee59ecb76a60e
arnaud.schieber <arnaud.schieber@etu.unistra.fr>
2021-02-05 17:13:28 +0100

    add User self, update, delete

--- backend/src/main/java/com/g6/acrobatteAPI/controllers/UserController.java
+++ backend/src/main/java/com/g6/acrobatteAPI/controllers/UserController.java
@@ -6,22 +6,30 @@ import com.g6.acrobatteAPI.entities.User;
 import com.g6.acrobatteAPI.repositories.UserRepository;
 import com.g6.acrobatteAPI.security.JwtTokenProvider;
 import com.g6.acrobatteAPI.entities.UserFactory;
+import com.g6.acrobatteAPI.hateoas.UserModelAssembler;
+import com.g6.acrobatteAPI.models.user.UserDeleteModel;
 import com.g6.acrobatteAPI.models.user.UserResponseModel;
 import com.g6.acrobatteAPI.models.user.UserSigninModel;
 import com.g6.acrobatteAPI.models.user.UserSignupModel;
+import com.g6.acrobatteAPI.models.user.UserUpdateModel;
 import com.g6.acrobatteAPI.services.UserService;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -30,22 +38,36 @@ public class UserController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
+    private final UserModelAssembler modelAssembler;
 
     UserController(UserService userService, AuthenticationManager authenticationManager,
-            JwtTokenProvider jwtTokenProvider, UserRepository userRepository) {
+            JwtTokenProvider jwtTokenProvider, UserRepository userRepository, UserModelAssembler modelAssembler) {
         this.userService = userService;
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
         this.userRepository = userRepository;
+        this.modelAssembler = modelAssembler;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // TODO: Vérifier les permissions ou changer les infos du retour
     public ResponseEntity<UserResponseModel> getUser(@PathVariable("id") Long id) {
         User user = userRepository.findById(id).get();
 
         UserResponseModel userResponse = userService.convertToResponseModel(user);
 
         return ResponseEntity.ok().body(userResponse);
+    }
+
+    @GetMapping("/self")
+    public ResponseEntity<EntityModel<UserResponseModel>> getSelf() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = ((UserDetails) principal).getUsername();
+
+        User user = userService.getUserByEmail(email);
+
+        UserResponseModel userResponse = userService.convertToResponseModel(user);
+
+        return ResponseEntity.ok().body(modelAssembler.toModel(userResponse, true));
     }
 
     @PostMapping("/signup")
@@ -58,7 +80,7 @@ public class UserController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<UserResponseModel> signin(@RequestBody @Valid UserSigninModel userSigninModel) {
+    public ResponseEntity<EntityModel<UserResponseModel>> signin(@RequestBody @Valid UserSigninModel userSigninModel) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(userSigninModel.getEmail(), userSigninModel.getPassword()));
 
@@ -72,6 +94,30 @@ public class UserController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", token);
 
-        return ResponseEntity.ok().headers(headers).body(userResponse);
+        return ResponseEntity.ok().headers(headers).body(modelAssembler.toModel(userResponse, true));
     }
+
+    @PutMapping("/self")
+    public ResponseEntity<EntityModel<UserResponseModel>> updateSelf(
+            @RequestBody @Valid UserUpdateModel userUpdateModel) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = ((UserDetails) principal).getUsername();
+
+        User user = userService.updateUser(email, userUpdateModel);
+
+        UserResponseModel userResponse = userService.convertToResponseModel(user);
+
+        return ResponseEntity.ok().body(modelAssembler.toModel(userResponse, true));
+    }
+
+    @DeleteMapping("/self")
+    public ResponseEntity<String> deleteSelf(@RequestBody @Valid UserDeleteModel userDeleteModel) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = ((UserDetails) principal).getUsername();
+
+        userService.deleteUser(email, userDeleteModel);
+
+        return ResponseEntity.ok("Compte supprimé");
+    }
+
 }
--- backend/src/main/java/com/g6/acrobatteAPI/services/UserService.java
+++ backend/src/main/java/com/g6/acrobatteAPI/services/UserService.java
@@ -3,14 +3,18 @@ package com.g6.acrobatteAPI.services;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.validation.Valid;
+
 import com.g6.acrobatteAPI.entities.Role;
 import com.g6.acrobatteAPI.entities.User;
+import com.g6.acrobatteAPI.models.user.UserDeleteModel;
 import com.g6.acrobatteAPI.models.user.UserResponseModel;
+import com.g6.acrobatteAPI.models.user.UserUpdateModel;
+import com.g6.acrobatteAPI.models.validators.ValidPassword;
 import com.g6.acrobatteAPI.repositories.UserRepository;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.expression.ParseException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +35,10 @@ public class UserService {
         return userRepository.getOne(id);
     }
 
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).get();
+    }
+
     public void createUser(User user) {
         try {
             String encodedPassword = passwordEncoder.encode(user.getPassword());
@@ -42,9 +50,57 @@ public class UserService {
         }
     }
 
+    public User updateUser(String email, UserUpdateModel userUpdateModel) {
+        User user = userRepository.findByEmail(email).get();
+
+        if (!passwordEncoder.matches(userUpdateModel.password, user.getPassword())) {
+            throw new IllegalArgumentException("Mot de passe incorrect");
+        }
+
+        user.setFirstName(userUpdateModel.firstName);
+        user.setName(userUpdateModel.name);
+
+        if (userUpdateModel.newPassword != null && userUpdateModel.newPasswordConfirmation != null) {
+
+            if (!userUpdateModel.newPassword.equals(userUpdateModel.newPasswordConfirmation)) {
+                throw new IllegalArgumentException(
+                        "Le mot de passe de confirmation doit correspondre au nouveau mot de passe.");
+            }
+
+            @ValidPassword
+            String validPassword = userUpdateModel.newPassword;
+            String encodedPassword = passwordEncoder.encode(validPassword);
+
+            user.setPassword(encodedPassword);
+        }
+
+        userRepository.save(user);
+
+        return user;
+    }
+
     public UserResponseModel convertToResponseModel(User user) {
         UserResponseModel userDTO = modelMapper.map(user, UserResponseModel.class);
+
         userDTO.setPassword(null);
+
         return userDTO;
+    }
+
+    public void deleteUser(String email, UserDeleteModel userDeleteModel) {
+        User user = userRepository.findByEmail(email).get();
+
+        try {
+            String encodedPassword = user.getPassword();
+
+            if (!passwordEncoder.matches(userDeleteModel.password, encodedPassword)) {
+                throw new IllegalArgumentException("Mot de passe incorrect");
+            }
+
+            userRepository.delete(user);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("Mot de passe incorrect");
+        }
     }
 }
