e32136b0aa9b863adaf339ae34fc972ace27b83a
arnaud.schieber <arnaud.schieber@etu.unistra.fr>
2021-02-19 00:08:09 +0100

    Fix paged assembler

--- backend/src/main/java/com/g6/acrobatteAPI/hateoas/ChallengeModelAssembler.java
+++ backend/src/main/java/com/g6/acrobatteAPI/hateoas/ChallengeModelAssembler.java
@@ -19,7 +19,7 @@ public class ChallengeModelAssembler
         public EntityModel<ChallengeResponseModel> toModel(ChallengeResponseModel challenge) {
                 EntityModel<ChallengeResponseModel> model = EntityModel.of(challenge);
                 model.add(linkTo(methodOn(ChallengeController.class).getChallenge(challenge.getId())).withSelfRel());
-                model.add(linkTo(methodOn(ChallengeController.class).getAllChallenges(PageRequest.of(0, 10)))
+                model.add(linkTo(methodOn(ChallengeController.class).pagedChallenges(PageRequest.of(0, 10)))
                                 .withRel("challenges"));
 
                 // HalModelBuilder model = HalModelBuilder.halModelOf(challenge);
