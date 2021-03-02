df2633b487dc7f575693cdcfbee63092f85a2195
Deewens <deeewens@gmail.com>
2021-03-02 19:32:45 +0100

    Add: coordinates list to GET Segments

--- backend/src/main/java/com/g6/acrobatteAPI/projections/segment/SegmentProjection.java
+++ backend/src/main/java/com/g6/acrobatteAPI/projections/segment/SegmentProjection.java
@@ -1,6 +1,10 @@
 package com.g6.acrobatteAPI.projections.segment;
 
+import com.g6.acrobatteAPI.entities.Coordinate;
+import com.g6.acrobatteAPI.entities.Endpoint;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
 
 public interface SegmentProjection {
 
@@ -10,4 +14,6 @@ public interface SegmentProjection {
     @Value("#{target.id}")
     public Long getId();
 
+    @Value("#{target.coordinates}")
+    List<Coordinate> getCoordinates();
 }
