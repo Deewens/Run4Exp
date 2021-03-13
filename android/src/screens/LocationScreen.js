79934b702d82b17042c68b0382a7e34000a6c8cb
arnaud.schieber <arnaud.schieber@etu.unistra.fr>
2021-03-13 11:56:20 +0100

    Add task manager

--- android/src/screens/LocationScreen.js
+++ android/src/screens/LocationScreen.js
@@ -6,11 +6,41 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Pedometer } from "expo-sensors";
 import Spacer from "../components/Spacer";
 import * as Location from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
 
 
 const LocationScreen = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+
+  const [updateMsg, setUpdateMsg] = useState(null);
+
+  const TASK_FETCH_LOCATION = 'acrobatt';
+
+  let start = () => {
+    // 2 start the task
+    Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
+      accuracy: Location.Accuracy.BestForNavigation,
+      distanceInterval: 5, // minimum change (in meters) betweens updates
+      deferredUpdatesInterval: 5000, // minimum interval (in milliseconds) between updates
+      // foregroundService is how you get the task to be updated as often as would be if the app was open
+      foregroundService: {
+        notificationTitle: 'Using your location',
+        notificationBody: 'To turn off, go back to the app and switch something off.',
+      },
+    });
+  }
+  
+
+  let stop = () => {
+  // 3 when you're done, stop it
+  Location.hasStartedLocationUpdatesAsync(TASK_FETCH_LOCATION).then((value) => {
+    if (value) {
+      Location.stopLocationUpdatesAsync(TASK_FETCH_LOCATION);
+    }
+  });
+  }
+
 
   useEffect(() => {
     (async () => {
@@ -20,8 +50,15 @@ const LocationScreen = () => {
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      // 1 define the task passing its name and a callback that will be called whenever the location changes
+      TaskManager.defineTask(TASK_FETCH_LOCATION, async ({ data: { locations }, error }) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+        const [locationtt] = locations;
+        setUpdateMsg(locationtt);
+      });
     })();
   }, []);
 
@@ -38,6 +75,11 @@ const LocationScreen = () => {
       <Text>
         {text}
       </Text>
+      <Text>
+      {updateMsg}
+      </Text>
+      <Button title="Start" onPress={start} />
+      <Button title="Stop" onPress={stop} />
     </View>
   );
 };
