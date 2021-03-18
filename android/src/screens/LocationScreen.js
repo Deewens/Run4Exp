615da50bdeefae53055f4e7bd543cf0b8725624d
arnaud.schieber <arnaud.schieber@etu.unistra.fr>
2021-03-18 09:30:46 +0100

    add meter counter

--- android/src/screens/LocationScreen.js
+++ android/src/screens/LocationScreen.js
@@ -3,37 +3,51 @@ import { Platform, Text, StyleSheet, View } from "react-native";
 import { Button } from "react-native-elements";
 import { Context as AuthContext } from "../context/AuthContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Pedometer } from "expo-sensors";
 import Spacer from "../components/Spacer";
 import * as Location from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
 
+const TASK_FETCH_LOCATION = 'acrobatt';
+
+TaskManager.defineTask(TASK_FETCH_LOCATION, async ({ data: { locations }, error }) => {
+  console.error("loc")
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const [locationtt] = locations;
+  setUpdateMsg(locations);
+});
 
 const LocationScreen = () => {
-  const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
-  const [updateMsg, setUpdateMsg] = useState(null);
+  const [updateMsg, setUpdateMsg] = useState("");
 
-  const TASK_FETCH_LOCATION = 'acrobatt';
+  const [running, setRunning] = useState(false);
+
+  const [meter, setMeter] = useState(0);
 
   let start = () => {
-    // 2 start the task
+    setMeter(0);
+    setRunning(true);
+
     Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
       accuracy: Location.Accuracy.BestForNavigation,
-      distanceInterval: 5, // minimum change (in meters) betweens updates
-      deferredUpdatesInterval: 5000, // minimum interval (in milliseconds) between updates
-      // foregroundService is how you get the task to be updated as often as would be if the app was open
+      distanceInterval: 1,
+      deferredUpdatesInterval: 1000,
+
       foregroundService: {
-        notificationTitle: 'Using your location',
-        notificationBody: 'To turn off, go back to the app and switch something off.',
+        notificationTitle: 'Location Activé',
+        notificationBody: 'Vous pouvez la désactivé depuis l\'application',
       },
     });
   }
   
 
   let stop = () => {
-  // 3 when you're done, stop it
+  setRunning(false);
+
   Location.hasStartedLocationUpdatesAsync(TASK_FETCH_LOCATION).then((value) => {
     if (value) {
       Location.stopLocationUpdatesAsync(TASK_FETCH_LOCATION);
@@ -50,36 +64,26 @@ const LocationScreen = () => {
         return;
       }
 
-      // 1 define the task passing its name and a callback that will be called whenever the location changes
-      TaskManager.defineTask(TASK_FETCH_LOCATION, async ({ data: { locations }, error }) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        const [locationtt] = locations;
-        setUpdateMsg(locationtt);
-      });
     })();
   }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
 
   return (
     <View style={styles.container}>
       <Text style={{ fontSize: 40 }}>Location</Text>
       <Text>
-        {text}
-      </Text>
-      <Text>
       {updateMsg}
       </Text>
-      <Button title="Start" onPress={start} />
-      <Button title="Stop" onPress={stop} />
+      {running ? 
+    (
+      <>
+      <Text style={{ fontSize: 30, textAlign:"center" }}>
+        {meter} m
+      </Text>
+    <Button title="Stop" onPress={stop} />
+    </>)  
+    :
+    (<Button title="Start" onPress={start} />)
+    }   
     </View>
   );
 };
