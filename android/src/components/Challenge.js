223b4e0a430a73c75063f1f1119f9989e03f5c1a
jonathanheintz <jonathan.heintz3@etu.unistra.fr>
2021-03-26 17:27:31 +0100

    correction bug

--- android/src/components/Challenge.js
+++ android/src/components/Challenge.js
@@ -3,12 +3,12 @@ import { Text, StyleSheet, View, Image, ScrollView } from "react-native";
 import Spacer from "../components/Spacer";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ChallengeApi from "../api/challenge.api";
-import {Context as AuthContext } from '../context/Auth  // let [nameCreator, setNameCreator] = useState([]);
-Context';
+import {Context as AuthContext } from '../context/AuthContext';
 
 const Challenge = (props) => {
   const {getToken} = useContext(AuthContext);
   let [token, setToken] = useState([]);
+  // let [nameCreator, setNameCreator] = useState([]);
 
   const readData = async () => {
     setToken(await getToken);
