272d0a18e27efb76702b17f391b4130fa8aea831
arnaud.schieber <arnaud.schieber@etu.unistra.fr>
2021-05-20 14:19:24 +0200

    Correction pagedList route

--- android/src/api/challenge.api.js
+++ android/src/api/challenge.api.js
@@ -2,7 +2,7 @@ import Api from './api';
 
 const ChallengeApi = {
   pagedList: async (pageNumber) => {
-    return await Api.get(`challenges?page=${pageNumber}&size=10&sort=id,desc`);
+    return await Api.get(`challenges/?page=${pageNumber}&size=10&sort=id,desc`);
   },
   get: async (id) => {
     return await Api.get(`challenges/${id}`);
