7f2f96fb42c0f9243d3f1a219de0e4792bbe5fbf
Adrien Dudon <adrien.dudon@etu.unistra.fr>
2021-06-03 15:32:54 +0200

    add deleteSelfUser keys as global var

--- webapp/src/api/queryKeys.ts
+++ webapp/src/api/queryKeys.ts
@@ -2,6 +2,7 @@ const queryKeys = {
   SELF_USER_SESSIONS: 'selfUserSessions',
   USER_SESSION: 'userSession',
   CHALLENGES: 'challenges',
+  DELETE_SELF_USER: 'deleteSelfUser'
 }
 
 export default queryKeys
