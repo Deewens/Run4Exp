4b51dfdb373d04c5b1419dd63f0c5e08eec84605
ukhanov <ilya.ukhanov@etu.unistra.fr>
2021-06-15 22:33:25 +0200

    Fix

--- backend/src/main/java/com/g6/acrobatteAPI/repositories/ChallengeRepository.java
+++ backend/src/main/java/com/g6/acrobatteAPI/repositories/ChallengeRepository.java
@@ -26,6 +26,7 @@ public interface ChallengeRepository extends PagingAndSortingRepository<Challeng
     Page<Challenge> findDistinctByAdministratorsInAndPublished(List<User> administrators, Boolean published,
             Pageable pageable);
 
-    @Query(value = "select ac from User u left join u.administeredChallenges ac where u = :admin ORDER BY ac.id")
-    Page<Challenge> findDistinctByAdministratorsInWithPagination(@Param("admin") User admin, Pageable pageable);
+    // @Query(value = "select c from Challenge c left join c.administrators a where
+    // a = :admin ORDER BY c.id")
+    Page<Challenge> findByAdministratorsIn(List<User> admins, Pageable pageable);
 }
