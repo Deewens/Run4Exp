9bd69f9e9ace26ce9b20ce6e740c82bcb47487be
jonathanheintz <jonathan.heintz3@etu.unistra.fr>
2021-06-16 11:06:01 +0200

    rework design dashboard

--- android/src/screens/DashboardScreen.js
+++ android/src/screens/DashboardScreen.js
@@ -31,26 +31,41 @@ const DashboardScreen = ({ navigation }) => {
                     <View style={styles.header}>
                         <View style={[styles.block, styles.presentation]}>
                             <View style={styles.buble}>
-                            <Avatar size={100} />
-                            <Text style={styles.title}>{state.user?.firstName} {state.user?.name}</Text>
+                                <Avatar size={100} />
+                                <Text style={styles.title}>{state.user?.firstName} {state.user?.name}</Text>
                             </View>
                         </View>
-                        <View style={[styles.block, styles.stat, styles.started]}><Text>6 challenges lancés</Text></View>
-                        <View style={[styles.block, styles.stat, styles.accomplish]}><Text>5 challenges accomplies</Text></View>
-                        <View style={[styles.block, styles.stat, styles.kilometer]}><Text>60km parcourus</Text></View>
-                        <View style={[styles.block, styles.stat, styles.time]}><Text>7h de courses</Text></View>
+
+                        <View style={[styles.block, styles.stat]}>
+                            <View style={styles.bubleStat}>
+                                <Text style={[styles.text, styles.valueStat]}>6</Text>
+                            </View>
+                            <Text style={[styles.text]}> challenges lancés</Text>
+                        </View>
+
+                        <View style={[styles.block, styles.stat]}>
+                            <View style={styles.bubleStat}>
+                                <Text style={[styles.text, styles.valueStat]}>5</Text>
+                            </View>
+                            <Text style={[styles.text]}> challenges accomplis</Text>
+                        </View>
+
+                        <View style={[styles.block, styles.stat]}>
+                            <View style={styles.bubleStat}>
+                                <Text style={[styles.text, styles.valueStat]}>60km</Text>
+                            </View>
+                            <Text style={[styles.text]}> parcourus</Text>
+                        </View>
+
+                        <View style={[styles.block, styles.stat]}>
+                            <View style={styles.bubleStat}>
+                                <Text style={[styles.text, styles.valueStat]}>7h</Text>
+                            </View>
+                            <Text style={[styles.text]}> de course</Text>
+                        </View>
                     </View>
-                
 
-                {/* <View style={styles.block} onPress={() => props.navigation.navigate('Home')}><Text>Commencer nouveau challenge</Text></View> */}
-
-
-
-                {/* {challengeList.length == 0 ? <Text style={styles.text}>Aucun challenge à présenter</Text> :
-          challengeList.map(function (challenge, key) {
-            return <ChallengeItem key={key} challenge={challenge} onPress={() => navChallenge(challenge.id)} />;
-          })} 
-            </ScrollView>*/}
+            {/*</ScrollView>*/}
             </View>
             </LinearGradient>
             </ThemedPage>
@@ -60,47 +75,34 @@ const DashboardScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
-        marginLeft: 0,
-        flex: 1,
-        width: 350,
-        height: 575,
     },
     header: {
         borderRadius: 10,
         display: 'flex',
-        alignItems: 'baseline',
-        elevation: 24,
-        height: 500,
+        height: '30%',
     },
     block: {
         display: 'flex',
-        height: 60,
-        flexDirection: 'column',
-        marginTop: 20,
-        alignItems: 'baseline',
-        elevation: 24,
-        justifyContent: 'center',
+        flexDirection: 'row',
+        height: '20%',
+        marginTop: 15,
         alignItems: 'center',
     },
     presentation: {
         display: 'flex',
-        backgroundColor: 'rgba(0, 0, 0,0.8)',
-        marginLeft: 26,
+        backgroundColor: 'rgba(0, 62, 59, 0.7)',
         borderRadius: 5,
         borderTopRightRadius: 43,
-        width: 298,
         height: 150,
-        elevation: 0,
         marginTop: 5,
     },
     buble:{
         alignItems: 'center',
         flexDirection: "row",
-        backgroundColor: 'rgba(200,200,200,0.3)',  
+        backgroundColor: 'rgba(255,255,255,0.3)',  
         borderRadius: 40,
         padding: 5,
-        borderBottomRightRadius: 70, 
-        width: 250,
+        width: '95%',
     },
     stat: {
         borderTopLeftRadius: 10,
@@ -108,29 +110,34 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: 5,
         borderBottomRightRadius: 5,
         marginBottom: 5,
-        height: 50,
-        width: 200,
-        marginLeft: 70,
-    },
-    kilometer: {
-        backgroundColor: 'rgba(100, 100, 255, 0.6)',
-    },
-    accomplish: {
-        backgroundColor: 'rgba(44, 242, 140, 0.6)',
-    },
-    started: {
-        backgroundColor: 'rgba(235, 40, 39, 0.6)',
-    },
-    time: {
-        backgroundColor: 'rgba(44, 242,100, 0.6)',
+        height: 70,
+        marginLeft: 10,
+        backgroundColor: 'rgba(0, 62, 59, 0.4)',
     },
     title: {
-        fontSize: 20,
+        fontSize: 18,
         color: 'white',
     },
     text: {
+        color: 'white',
         fontSize: 20,
-    }
+        marginLeft: 10,
+    },
+    bubleStat:{
+        height: '100%',
+        width: '23%',
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 5,
+        backgroundColor: 'white',
+        justifyContent: 'center', 
+        alignItems: 'center',
+        borderWidth: 0.5,
+    },
+    valueStat:{
+        fontSize: 18,
+        color: 'black',
+        fontWeight: 'bold',
+    },
 });
 
 export default DashboardScreen;
